e7554688d1d9a0053dd66058d5c0c4d8612cfbda
IssaKamara958 <122808239+IssaKamara958@users.noreply.github.com>
2024-10-28 01:16:20 +0000

    Update test.js

--- test.js
+++ test.js
@@ -9,3 +9,18 @@ document.addEventListener("DOMContentLoaded", function() {
         }, 100); // Délai pour commencer l'animation
     });
 });
+
+document.getElementById('download-btn').addEventListener('click', function () {
+    const element = document.body; // L'élément que vous voulez convertir en PDF
+    html2pdf()
+        .set({
+            filename: 'CV_Issa_Kamara.pdf',
+            margin: 0,
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+        })
+        .from(element)
+        .save();
+});
+
